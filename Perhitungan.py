954cc3c6d97b33c9d02fae09cd5b4207c9aecc75
tasyaulfha <17523184@students.uii.ac.id>
2021-02-16 21:45:27 +0700

    begginer

---
+++ Perhitungan.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(4+5)
+
+
+# In[2]:
+
+
+print(9*7)
+
+
+# In[3]:
+
+
+print(-1/4)
+
+
+# In[4]:
+
+
+print(1/3)
+
+
+# In[5]:
+
+
+print(((2050/5)-32)/9)
+
+
+# In[6]:
+
+
+print(2**10)
+
+
+# In[ ]:
+
+
+
+
---
+++ pertama.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+for i in range(10):
+    print("Hello world")
+    
+
+
+# In[3]:
+
+
+print("hello world")
+
+
+# In[7]:
+
+
+color = "Green"
+thing = "hope"
+print(color + " is the color of " + thing)
+
+
+# In[ ]:
+
+
+
+
