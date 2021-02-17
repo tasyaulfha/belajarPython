1b8a9e3c117203f56d5c8e9c00cc585ce7c3b3a2
tasyaulfha <17523184@students.uii.ac.id>
2021-02-17 15:27:29 +0700

    Membuat fungsi

---
+++ fungsi.py
@@ -0,0 +1,11 @@
+def greeting(name):
+    print("welcome, " +name)
+
+greeting("Kay")
+greeting("Sya")
+
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+
+greeting("tasya", "informatics")
