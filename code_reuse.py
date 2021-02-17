7a557efdd453e9dcc4c93a3b8ca6edf53b958d54
tasyaulfha <17523184@students.uii.ac.id>
2021-02-17 16:22:20 +0700

    kuis fungsi

---
+++ code_reuse.py
@@ -0,0 +1,5 @@
+# REPLACE THIS STARTER CODE WITH YOUR FUNCTION
+june_days = 30
+print("June has " + str(june_days) + " days.")
+july_days = 31
+print("July has " + str(july_days) + " days.")
---
+++ reuse.py
@@ -0,0 +1,6 @@
+def lucky_number(name):
+    number = len(name)*9
+    print("Hello, "+ name + " Your lucky number is :" + str(number))
+
+lucky_number ("Kay")
+lucky_number("tasya")
